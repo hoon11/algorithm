13b69faad14bc13b164892ac1b5ff42081ec9fbd
hoon11 <jhoonen@gmail.com>
2021-10-03 16:58:31 +0900

    Fix Lab 4

--- Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
+++ Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
@@ -16,54 +16,51 @@ public final class Cracker {
 
     public String[] run(final RainbowTable[] rainbowTables) {
         String[] plainTexts = new String[this.userTable.length];
-        int userI = 0;
+
         int hashAlgorithmIndex = -1;
-        for (userI = 0; userI < this.userTable.length; userI++) {
+        for (int userI = 0; userI < this.userTable.length; userI++) {
             String userPasswordHash = this.userTable[userI].getPasswordHash();
-            String userEmail = this.userTable[userI].getEmail();
-
-            if (hashAlgorithmIndex != -1) {
-                plainTexts[userI] = rainbowTables[hashAlgorithmIndex].get(userPasswordHash);
-
-                continue;
-            }
 
             String passwordCRC32 = rainbowTables[0].get(userPasswordHash);
             if (passwordCRC32 != null) {
-                plainTexts[userI] = passwordCRC32;
                 hashAlgorithmIndex = 0;
-                continue;
+                break;
             }
 
             String passwordMD2 = rainbowTables[1].get(userPasswordHash);
             if (passwordMD2 != null) {
-                plainTexts[userI] = passwordMD2;
                 hashAlgorithmIndex = 1;
-                continue;
+                break;
             }
 
             String passwordMD5 = rainbowTables[2].get(userPasswordHash);
             if (passwordMD5 != null) {
-                plainTexts[userI] = passwordMD5;
                 hashAlgorithmIndex = 2;
-                continue;
+                break;
             }
 
             String passwordSHA1 = rainbowTables[3].get(userPasswordHash);
             if (passwordSHA1 != null) {
-                plainTexts[userI] = passwordSHA1;
                 hashAlgorithmIndex = 3;
-                continue;
+                break;
             }
 
             String passwordSHA265 = rainbowTables[4].get(userPasswordHash);
             if (passwordSHA265 != null) {
-                plainTexts[userI] = passwordSHA265;
                 hashAlgorithmIndex = 4;
-                continue;
+                break;
             }
         }
 
+        if (hashAlgorithmIndex == -1) {
+            return plainTexts;
+        }
+
+        for (int userI = 0; userI < this.userTable.length; userI++) {
+            String userPasswordHash = this.userTable[userI].getPasswordHash();
+            plainTexts[userI] = rainbowTables[hashAlgorithmIndex].get(userPasswordHash);
+        }
+        
         return plainTexts;
     }
 }
