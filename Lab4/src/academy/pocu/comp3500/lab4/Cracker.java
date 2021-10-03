fefec2a8e13c512dff840f7a60740f387c86d9aa
hoon11 <jhoonen@gmail.com>
2021-10-03 17:19:23 +0900

    Fix Lab 4

--- Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
+++ Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
@@ -16,49 +16,23 @@ public final class Cracker {
 
     public String[] run(final RainbowTable[] rainbowTables) {
         String[] plainTexts = new String[this.userTable.length];
-
         int hashAlgorithmIndex = -1;
-        for (int userI = 0; userI < this.userTable.length; userI++) {
-            String userPasswordHash = this.userTable[userI].getPasswordHash();
-
-            String passwordCRC32 = rainbowTables[0].get(userPasswordHash);
-            if (passwordCRC32 != null) {
-                hashAlgorithmIndex = 0;
-                break;
-            }
-
-            String passwordMD2 = rainbowTables[1].get(userPasswordHash);
-            if (passwordMD2 != null) {
-                hashAlgorithmIndex = 1;
-                break;
-            }
-
-            String passwordMD5 = rainbowTables[2].get(userPasswordHash);
-            if (passwordMD5 != null) {
-                hashAlgorithmIndex = 2;
-                break;
-            }
-
-            String passwordSHA1 = rainbowTables[3].get(userPasswordHash);
-            if (passwordSHA1 != null) {
-                hashAlgorithmIndex = 3;
-                break;
-            }
-
-            String passwordSHA265 = rainbowTables[4].get(userPasswordHash);
-            if (passwordSHA265 != null) {
-                hashAlgorithmIndex = 4;
-                break;
+        int j = 0;
+        for (int i = 0; i < this.userTable.length; i++) {
+            while (j < rainbowTables.length && hashAlgorithmIndex == -1) {
+                if (rainbowTables[j].get(this.userTable[i].getPasswordHash())) {
+                    hashAlgorithmIndex = j;
+                }
+                j++;
             }
         }
 
-        if (hashAlgorithmIndex == -1) {
+        if (hashAlgorithmIndex != -1) {
             return plainTexts;
         }
 
-        for (int userI = 0; userI < this.userTable.length; userI++) {
-            String userPasswordHash = this.userTable[userI].getPasswordHash();
-            plainTexts[userI] = rainbowTables[hashAlgorithmIndex].get(userPasswordHash);
+        for (int i = 0; i < this.userTable.length; i++) {
+            plainTexts[i] = rainbowTables[hashAlgorithmIndex].get(this.userTable[i].getPasswordHash());
         }
         
         return plainTexts;
